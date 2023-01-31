c8079a038d8cb7d6ae94fa80c5e811ccf3ae40c2
Mauricio <maudusi12@gmail.com>
2023-01-30 21:18:36 -0500

    adding complete exercise files

---
+++ app.js
@@ -0,0 +1,42 @@
+//1
+function hasOddNumber(arr) {
+  return arr.some((num) => {
+    return num % 2 !== 0;
+  });
+}
+
+//2 NOT SOLVED?
+function hasAZero(num) {
+  let arr = Array.from(num);
+  return arr.some(function (val) {
+    return arr.parseInt().includes(0);
+  });
+}
+
+//3
+function hasOnlyOddNumbers(arr) {
+  return arr.every((numsOfArr) => {
+    return numsOfArr % 2 !== 0;
+  });
+}
+
+//4
+function hasNoDuplicates(arr) {
+  return arr.every((val) => {
+    return arr.indexOf(val) === arr.indexOf(val);
+  });
+}
+
+//5
+function hasCertainKey(obj, key) {
+  return obj.every((object) => {
+    return key in object;
+  });
+}
+
+//6
+function hasCertainValue(obj, key, searchValue) {
+  return arr.every((object) => {
+    return object[key] === searchValue;
+  });
+}
